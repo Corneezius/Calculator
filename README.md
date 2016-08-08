683b5bb3363e2bf563acb9d8db0fc79f8e3ac0b1
Alexandre Leibler and Kyle Fisher <alexandre+kyle@>
2016-08-08 13:53:01 -0400

    add README.md and import script from js-fiddle

---
+++ README.md
@@ -0,0 +1,34 @@
+# _CALCULATOR_
+
+#### _Calculator with front end and backend, 08.08.2016_
+
+#### By _**Alexandre Leibler, Kyle Fisher**_
+
+## DESCRIPTION
+_This is a calculator project designed to teach us about javascript_
+
+## SETUP/INSTALLATION REQUIREMENTS
+
+* _Just clone the project from github_
+* _Use as you would any other web app calculator_
+
+## KNOWN BUGS
+
+* _No none bugs_
+
+## SUPPORT AND CONTACT DETAILS
+
+* _For further support, contact us at the provided e-mails._
+
+## TECHNOLOGIES USED
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+* _Bootstrap_
+* _Mathmatics_
+
+### License
+
+Copyright (c) 2016 **_Alex Leibler, Kyle Fisher_**
+
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter your first number:"));
+var number2 = parseInt(prompt("Enter your second number:"));
+var result = add(number1, number2);
+alert(result);
+
+var remainder = function() {
+	var number1 = prompt("Your first integer, sir?");
+  var number2 = prompt("Your second integer, sir?");
+
+  number1 = parseInt(number1);
+  number2 = parseInt(number2);
+
+ 	var divisionresult = (number1 / number2);
+ 	var parsedresult = parseInt(divisionresult);
+  var multiresult = (parsedresult * number2);
+ 	var result = (number1 - multiresult);
+  alert("The remainder of " + number1 +" and " + number2 + " is " + result);
+
+}
+
+remainder();
