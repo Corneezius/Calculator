cc09178a83f4adafe9b9259b10feb5a8c5f94870
Kyle Fisher, Meredith Alcorn and Peter Armington <peter@>
2016-08-10 14:43:40 -0400

    reflect final revisions to a calculator capable of multiplying dividing adding and subtracting through use of buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,39 @@ var divide = function(number1, number2) {
 //UI Logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("#addButton").click(function(event){
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
 
     var result = add(number1, number2);
-    $("#output").text(result)
-  })
+    $("#output").text(result);
+  });
 
-})
+  $("#subtractButton").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("#multiplyButton").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("#divideButton").click(function(event){
+    console.log("divideButton")
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+
+    var result = divide(number1, number2);
+    $("#output").text("= " + result);
+  });
+});
