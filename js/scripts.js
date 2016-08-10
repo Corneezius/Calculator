696b67bcb63f3fdc323d2f134837a03c8eb1f859
Kyle Fisher, Meredith Alcorn and Peter Armington <peter@>
2016-08-10 11:43:43 -0400

    change the existing calculator app so that results are displayed on the page rather than through alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,30 @@
+// Backend Logic
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter your first number:"));
-var number2 = parseInt(prompt("Enter your second number:"));
-var result = add(number1, number2);
-alert(result);
-
-/*
-var remainder = function() {
-	var number1 = prompt("Your first integer, sir?");
-  var number2 = prompt("Your second integer, sir?");
-
-  number1 = parseInt(number1);
-  number2 = parseInt(number2);
-
- 	var divisionresult = (number1 / number2);
- 	var parsedresult = parseInt(divisionresult);
-  var multiresult = (parsedresult * number2);
- 	var result = (number1 - multiresult);
-  alert("The remainder of " + number1 +" and " + number2 + " is " + result);
-
-}
-*/
-
 var subtract = function(number1, number2) {
   return number1 - number2;
-}
+};
 
 var multiply = function(number1, number2) {
   return number1 * number2;
-}
+};
 
 var divide = function(number1, number2) {
   return number1 / number2;
-}
+};
 
-remainder();
+//UI Logic
+
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+
+    var result = add(number1, number2);
+    $("#output").text(result)
+  })
+
+})
